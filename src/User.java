7eb301d3fa1bc6656f719d4260076af895f6c8ca
Agrim Jain <agrimjain056@gmail.com>
2026-02-18 14:36:31 +0530

    Add user model class with file serialization

---
+++ src/User.java
@@ -0,0 +1,25 @@
+public class User {
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String username, String password, String role)
+    {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
+
+    public  String toFileString() 
+    {
+        return username + "," + password + "," + role;
+    }
+    public static User fromFileStrng(String line)
+    {
+        String[] parts = line.split(",");
+        return new User (parts[0], parts[1], parts[2]);
+    }
+}
